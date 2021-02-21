602f98ce11f8e1a2147f08b9f2213a0ae90c3b2d
felix <felix@webisoft.com>
2021-02-21 16:28:39 -0500

    put all sections in components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,23 @@
 import * as React from 'react'
-import {Container, Row, Button} from 'reactstrap'
-import {Link} from 'gatsby'
-import StyledHeader from '../components/StyledHeader'
-import Avatar from '../components/Avatar'
 import DiagonalBackground from '../images/diagonal-background.svg'
-import {
-  Title,
-  Subtitle,
-  Main,
-  AvatarColumn,
-  IntroSection,
-  AboutSection,
-  ProjectsSection,
-  ContactSection,
-  TextColumn,
-  BigText,
-  ButtonContainer,
-} from '../styles/styled-components'
+import IntroSection from '../components/IntroSection'
+import AboutSection from '../components/AboutSection'
+import ProjectsSection from '../components/ProjectsSection'
+import ContactSection from '../components/ContactSection'
+import {Main} from '../styles/styled-components'
 
 function IndexPage() {
   return (
     <Main bg={DiagonalBackground}>
       <title>Home Page</title>
-      <IntroSection id="intro">
-        <StyledHeader>
-          <Title>Felix Rioux Sabourin</Title>
-        </StyledHeader>
-        <Container>
-          <Row>
-            <TextColumn>
-              <BigText>
-                Web developer
-                <span>Portfolio</span>
-              </BigText>
-              <ButtonContainer>
-                <Link className="btn btn-blue" to="#contact">
-                  Contact
-                </Link>
-                <Link className="btn btn-yellow" to="#projects">
-                  Projects
-                </Link>
-              </ButtonContainer>
-            </TextColumn>
-            <AvatarColumn>
-              <Avatar />
-            </AvatarColumn>
-          </Row>
-        </Container>
-      </IntroSection>
 
-      <AboutSection id="about">
-        <StyledHeader>
-          <Subtitle>About Me</Subtitle>
-        </StyledHeader>
-      </AboutSection>
+      <IntroSection />
 
-      <ProjectsSection id="projects">
-        <StyledHeader>
-          <Subtitle>Projects</Subtitle>
-        </StyledHeader>
-      </ProjectsSection>
+      <AboutSection />
 
-      <ContactSection id="contact">
-        <StyledHeader>
-          <Subtitle>Contact</Subtitle>
-        </StyledHeader>
-      </ContactSection>
+      <ProjectsSection />
+
+      <ContactSection />
     </Main>
   )
 }
