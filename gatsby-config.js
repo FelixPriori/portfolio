075f930a50997792e66b7cd71f88eb911d16c77f
felix <felix@webisoft.com>
2021-02-21 13:04:15 -0500

    add google fonts, sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: "portfolio",
+    title: 'portfolio',
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"],
-};
+  plugins: [
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+      },
+    },
+    `gatsby-plugin-sass`,
+  ],
+}
