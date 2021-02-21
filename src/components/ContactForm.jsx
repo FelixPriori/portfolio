b499095dabd088e2913958c287acf052929148ca
felix <felix@webisoft.com>
2021-02-21 17:26:18 -0500

    basic contact form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Form, Input, Label, Button} from 'reactstrap'
+
+function ContactForm() {
+  return (
+    <Form>
+      <Label htmlFor="name">Name</Label>
+      <Input name="name" type="text" />
+
+      <Label htmlFor="email">Email</Label>
+      <Input name="email" type="email" />
+
+      <Label htmlFor="subject">Subject</Label>
+      <Input name="subject" type="text" />
+
+      <Label htmlFor="message">message</Label>
+      <Input name="message" type="textarea" />
+
+      <Button color="yellow" type="submit">
+        Send
+      </Button>
+    </Form>
+  )
+}
+
+export default ContactForm
