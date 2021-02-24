3e10cb12726a6aefc2c6e1077cf8050aff4f1003
felix <felix@webisoft.com>
2021-02-24 11:28:48 -0500

    remove name

--- src/components/SocialList.jsx
+++ src/components/SocialList.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import {Row, Col} from 'reactstrap'
 import {ReactComponent as GitHub} from '../images/logo-github.svg'
 import {ReactComponent as Instagram} from '../images/logo-instagram.svg'
 import {ReactComponent as LinkedIn} from '../images/logo-linkedin.svg'
 import {ReactComponent as Twitter} from '../images/logo-twitter.svg'
 import {List, ListItem, StyledIconLink} from '../styles/styled-components'
 
-function SocialListItem({link, name, children}) {
+function SocialListItem({link, children}) {
   return (
     <ListItem>
       <StyledIconLink href={link} target="_blank" rel="noreferrer">
         {children}
-        {name}
       </StyledIconLink>
     </ListItem>
   )
@@ -19,28 +17,20 @@ function SocialListItem({link, name, children}) {
 
 function SocialList() {
   return (
-    <Row>
-      <Col>
-        <List>
-          <SocialListItem link="https://www.linkedin.com/in/felix-rioux-sabourin/" name="LinkedIn">
-            <LinkedIn />
-          </SocialListItem>
-          <SocialListItem link="https://github.com/FelixPriori/" name="GitHub">
-            <GitHub />
-          </SocialListItem>
-        </List>
-      </Col>
-      <Col>
-        <List>
-          <SocialListItem link="https://twitter.com/FelixPriori" name="Twitter">
-            <Twitter />
-          </SocialListItem>
-          <SocialListItem link="https://www.instagram.com/felixpriori/" name="Instagram">
-            <Instagram />
-          </SocialListItem>
-        </List>
-      </Col>
-    </Row>
+    <List>
+      <SocialListItem link="https://www.linkedin.com/in/felix-rioux-sabourin/">
+        <LinkedIn />
+      </SocialListItem>
+      <SocialListItem link="https://github.com/FelixPriori/">
+        <GitHub />
+      </SocialListItem>
+      <SocialListItem link="https://twitter.com/FelixPriori">
+        <Twitter />
+      </SocialListItem>
+      <SocialListItem link="https://www.instagram.com/felixpriori/">
+        <Instagram />
+      </SocialListItem>
+    </List>
   )
 }
 
