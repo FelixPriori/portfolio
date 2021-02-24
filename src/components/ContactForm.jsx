9dcad8c72d36b19749a29b7d2a4c5b98e992cac9
felix <felix@webisoft.com>
2021-02-23 19:57:26 -0500

    relocate styled-components

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react'
-import styled from 'styled-components'
 import {useForm} from 'react-hook-form'
 import {navigate} from 'gatsby'
 import {yupResolver} from '@hookform/resolvers/yup'
 import axios from 'axios'
 import * as yup from 'yup'
-import {Form, Input, Label, Button, FormGroup} from 'reactstrap'
+import {Form, Label, Button, FormGroup} from 'reactstrap'
+import {ErrorMessage, StyledInput, StyledTextarea} from '../styles/styled-components'
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -114,38 +114,3 @@ function ContactForm() {
 }
 
 export default ContactForm
-
-const ErrorMessage = styled.div`
-  color: #bf616a;
-`
-const StyledInput = styled.input`
-  display: block;
-  width: 100%;
-  height: calc(1.5em + 0.75rem + 2px);
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  font-weight: 400;
-  line-height: 1.5;
-  color: #495057;
-  background-color: #fff;
-  background-clip: padding-box;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-`
-
-const StyledTextarea = styled.textarea`
-  display: block;
-  width: 100%;
-  min-height: calc(1.5em + 0.75rem + 2px);
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  font-weight: 400;
-  line-height: 1.5;
-  color: #495057;
-  background-color: #fff;
-  background-clip: padding-box;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-`
