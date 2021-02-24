e87e1532e6fb009a9ac987f2ef628bc3c0014744
felix <felix@webisoft.com>
2021-02-23 19:57:13 -0500

    add max-width

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
 import AvatarImage from '../images/avatar.jpg'
+import {StyledAvatar} from '../styles/styled-components'
 
 function Avatar() {
   return <StyledAvatar src={AvatarImage} alt="Felix Headshot" />
 }
 
 export default Avatar
-
-const StyledAvatar = styled.img`
-  border-radius: 50%;
-`
