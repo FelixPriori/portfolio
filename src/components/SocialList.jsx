7eefdacf964cec27e476413a6c17395b0ca6d686
felix <felix@webisoft.com>
2021-02-23 19:58:34 -0500

    import svg as components, change color

--- src/components/SocialList.jsx
+++ src/components/SocialList.jsx
@@ -1,65 +1,47 @@
 import React from 'react'
-import styled from 'styled-components'
-import GitHub from '../images/logo-github.svg'
-import Instagram from '../images/logo-instagram.svg'
-import LinkedIn from '../images/logo-linkedin.svg'
-import Twitter from '../images/logo-twitter.svg'
+import {Row, Col} from 'reactstrap'
+import {ReactComponent as GitHub} from '../images/logo-github.svg'
+import {ReactComponent as Instagram} from '../images/logo-instagram.svg'
+import {ReactComponent as LinkedIn} from '../images/logo-linkedin.svg'
+import {ReactComponent as Twitter} from '../images/logo-twitter.svg'
+import {List, ListItem, StyledIconLink} from '../styles/styled-components'
 
-function SocialIcon({icon}) {
-  return <Icon src={icon} />
-}
-
-function SocialListItem({icon, link, name}) {
+function SocialListItem({link, name, children}) {
   return (
     <ListItem>
-      <StyledLink href={link} target="_blank" rel="noreferrer">
-        <SocialIcon icon={icon} />
+      <StyledIconLink href={link} target="_blank" rel="noreferrer">
+        {children}
         {name}
-      </StyledLink>
+      </StyledIconLink>
     </ListItem>
   )
 }
 
 function SocialList() {
   return (
-    <List>
-      <SocialListItem icon={LinkedIn} link="https://www.linkedin.com/in/felix-rioux-sabourin/" name="LinkedIn" />
-      <SocialListItem icon={GitHub} link="https://github.com/FelixPriori/" name="GitHub" />
-      <SocialListItem icon={Twitter} link="https://twitter.com/FelixPriori" name="Twitter" />
-      <SocialListItem icon={Instagram} link="https://www.instagram.com/felixpriori/" name="Instagram" />
-    </List>
+    <Row>
+      <Col>
+        <List>
+          <SocialListItem link="https://www.linkedin.com/in/felix-rioux-sabourin/" name="LinkedIn">
+            <LinkedIn />
+          </SocialListItem>
+          <SocialListItem link="https://github.com/FelixPriori/" name="GitHub">
+            <GitHub />
+          </SocialListItem>
+        </List>
+      </Col>
+      <Col>
+        <List>
+          <SocialListItem link="https://twitter.com/FelixPriori" name="Twitter">
+            <Twitter />
+          </SocialListItem>
+          <SocialListItem link="https://www.instagram.com/felixpriori/" name="Instagram">
+            <Instagram />
+          </SocialListItem>
+        </List>
+      </Col>
+    </Row>
   )
 }
 
 export default SocialList
-
-const List = styled.ul`
-  list-style: none;
-  margin-left: 0;
-`
-
-const ListItem = styled.li`
-  margin: 1em 0;
-`
-
-const StyledLink = styled.a`
-  color: #5e81ac;
-  font-size: 20px;
-  transition: 200ms;
-
-  &:hover {
-    text-decoration: none;
-    font-size: 24px;
-
-    img {
-      height: 36px;
-      width: 36px;
-    }
-  }
-`
-
-const Icon = styled.img`
-  width: 32px;
-  height: 32px;
-  margin-right: 1em;
-`
