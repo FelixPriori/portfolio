bca4bae465dbf36abc042bde362122fd8d0a3396
felix <felix@webisoft.com>
2021-02-23 19:55:52 -0500

    scrap highlight

--- src/components/StyledHeader.jsx
+++ src/components/StyledHeader.jsx
@@ -1,33 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
+import {HeaderContainer} from '../styles/styled-components'
 
 function StyledHeader({children}) {
-  return (
-    <Container>
-      <TitleBox>{children}</TitleBox>
-      <HighlightBox />
-    </Container>
-  )
+  return <HeaderContainer>{children}</HeaderContainer>
 }
 
 export default StyledHeader
-
-const TitleBox = styled.div`
-  height: 50px;
-`
-
-const HighlightBox = styled.div`
-  background-color: #ebcb8b;
-  height: 30px;
-  position: relative;
-  left: 25px;
-  top: -25px;
-  z-index: -1;
-  border-radius: 5px;
-`
-
-const Container = styled.div`
-  height: 75px;
-  width: fit-content;
-  margin: 1em;
-`
