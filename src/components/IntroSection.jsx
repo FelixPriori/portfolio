4449389fb84990083d517319ee3b0bbfa14405e5
felix <felix@webisoft.com>
2021-02-23 19:57:38 -0500

    review responsive

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import {Link} from 'gatsby'
 import {Container, Row} from 'reactstrap'
 import StyledHeader from './StyledHeader'
@@ -8,35 +7,31 @@ import {TextColumn, Title, BigText, ButtonContainer, AvatarColumn} from '../styl
 
 function IntroSection() {
   return (
-    <IntroSectionWrapper id="intro">
-      <Container>
-        <StyledHeader>
-          <Title>Felix Rioux Sabourin</Title>
-        </StyledHeader>
-        <Row>
-          <TextColumn>
-            <BigText>
-              Web developer
-              <span>Portfolio</span>
-            </BigText>
-            <ButtonContainer>
-              <Link className="btn btn-blue" to="#contact">
-                Contact
-              </Link>
-              <Link className="btn btn-yellow" to="#projects">
-                Projects
-              </Link>
-            </ButtonContainer>
-          </TextColumn>
-          <AvatarColumn>
-            <Avatar />
-          </AvatarColumn>
-        </Row>
-      </Container>
-    </IntroSectionWrapper>
+    <Container id="intro">
+      <StyledHeader>
+        <Title>Felix Rioux Sabourin</Title>
+      </StyledHeader>
+      <Row>
+        <TextColumn>
+          <BigText>
+            Web developer
+            <span>Portfolio</span>
+          </BigText>
+          <ButtonContainer>
+            <Link className="btn btn-blue" to="#contact">
+              Contact
+            </Link>
+            <Link className="btn btn-yellow" to="#projects">
+              Projects
+            </Link>
+          </ButtonContainer>
+        </TextColumn>
+        <AvatarColumn>
+          <Avatar />
+        </AvatarColumn>
+      </Row>
+    </Container>
   )
 }
 
 export default IntroSection
-
-const IntroSectionWrapper = styled.section``
