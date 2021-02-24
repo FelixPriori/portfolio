58ebd9c474b7c150c2d15c172e393bbaf5c1be89
felix <felix@webisoft.com>
2021-02-23 19:57:00 -0500

    review responsive and form

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
 import {Container, Row, Col} from 'reactstrap'
 import ContactForm from './ContactForm'
 import SocialList from './SocialList'
-import {Subtitle, SocialMediaColumn} from '../styles/styled-components'
+import {Subtitle, SocialMediaColumn, ContactSectionWrapper, TitleContainer} from '../styles/styled-components'
 import ContactBackground from '../images/contact-background.svg'
 
 function ContactSection() {
@@ -11,7 +10,7 @@ function ContactSection() {
     <ContactSectionWrapper id="contact" bg={ContactBackground}>
       <Container>
         <TitleContainer>
-          <Subtitle color="#eceff4">Contact</Subtitle>
+          <Subtitle isContact>Contact</Subtitle>
         </TitleContainer>
 
         <Row>
@@ -30,24 +29,3 @@ function ContactSection() {
 }
 
 export default ContactSection
-
-const ContactSectionWrapper = styled.section`
-  padding: 4em 0;
-  color: #eceff4;
-  ${(props) =>
-    props.bg &&
-    css`
-      background-image: url(${props.bg});
-      background-attachment: scroll;
-      background-position: center;
-      background-repeat: no-repeat;
-    `}
-
-  a {
-    color: #eceff4;
-  }
-`
-
-const TitleContainer = styled.div`
-  padding: 2em 0;
-`
