63954cf20f0f091a55b9e2ac7798d3cbc4d82c88
felix <felix@webisoft.com>
2021-02-24 19:41:32 -0500

    fix imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import IntroSection from '../components/IntroSection'
-import AboutSection from '../components/AboutSection'
-import ProjectsSection from '../components/ProjectsSection'
+import IntroSection from '../components/index-components/IntroSection'
+import AboutSection from '../components/index-components/AboutSection'
+import ProjectsSection from '../components/index-components/ProjectsSection'
 import ContactSection from '../components/ContactSection'
 import {Main} from '../styles/styled-components'
 
