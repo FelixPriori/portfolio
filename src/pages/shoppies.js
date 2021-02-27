fab27430e3ee9a98318311f2bd042fdc751338b1
felix <felix@webisoft.com>
2021-02-27 16:41:11 -0500

    add all elements to shoppies

--- src/pages/shoppies.js
+++ src/pages/shoppies.js
@@ -4,13 +4,22 @@ import ContactSection from '../components/ContactSection'
 import IntroSection from '../components/shoppies-components/IntroSection'
 import SummarySection from '../components/shoppies-components/SummarySection'
 import StackSection from '../components/shoppies-components/StackSection'
+import ExtraFeaturesSection from '../components/shoppies-components/ExtraFeaturesSection'
+import ConclusionSection from '../components/shoppies-components/ConclusionSection'
 
 function ShoppiesPage() {
   return (
     <Main>
       <IntroSection />
+
       <SummarySection />
-      {/* <StackSection /> */}
+
+      <StackSection />
+
+      <ExtraFeaturesSection />
+
+      <ConclusionSection />
+
       <ContactSection />
     </Main>
   )
