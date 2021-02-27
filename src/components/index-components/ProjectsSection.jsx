8dfd431ab56e05c0488adfac31bf8fd80f6b892b
felix <felix@webisoft.com>
2021-02-27 16:34:14 -0500

    link to shoppies, comment out sidequests

--- src/components/index-components/ProjectsSection.jsx
+++ src/components/index-components/ProjectsSection.jsx
@@ -4,7 +4,7 @@ import Project from './Project'
 import StyledHeader from '../StyledHeader'
 import {Subtitle, ProjectsSectionWrapper} from '../../styles/styled-components'
 import Shoppies from '../../images/shoppies.png'
-import SideQuests from '../../images/side-quests.png'
+// import SideQuests from '../../images/side-quests.png'
 
 const shoppiesStack = [
   {tech: 'React', id: 1},
@@ -15,14 +15,14 @@ const shoppiesStack = [
   {tech: 'OMDb API', id: 6},
 ]
 
-const sideQuestsStack = [
-  {tech: 'React', id: 1},
-  {tech: 'Sass', id: 2},
-  {tech: 'Bootstrap', id: 3},
-  {tech: 'Expressjs', id: 4},
-  {tech: 'Nodejs', id: 5},
-  {tech: 'PostgreSQL', id: 6},
-]
+// const sideQuestsStack = [
+//   {tech: 'React', id: 1},
+//   {tech: 'Sass', id: 2},
+//   {tech: 'Bootstrap', id: 3},
+//   {tech: 'Expressjs', id: 4},
+//   {tech: 'Nodejs', id: 5},
+//   {tech: 'PostgreSQL', id: 6},
+// ]
 
 function ProjectsSection() {
   return (
@@ -34,18 +34,18 @@ function ProjectsSection() {
         <Project
           name="Shoppies"
           description="Shopify has branched out into movie award shows and we need your help. Please build us an app to help manage our movie nominations for the upcoming Shoppies."
-          slug="/under-construction"
+          slug="/shoppies"
           screenshot={Shoppies}
           stack={shoppiesStack}
         />
-        <Project
+        {/* <Project
           name="Side Quests"
           description="SideQuests is a web app which facilitates helping people in need, solving current world issues on a small scale with a fun fantasy theme. As an adventurer, you can pick a class, view what villager requests are available to you in your area, and complete them to earn badges, and level up your character. As a villager, you can post a request, and get the help of an adventurer in your area depending on the request type."
           slug="/under-construction"
           screenshot={SideQuests}
           stack={sideQuestsStack}
           alt
-        />
+        /> */}
       </Container>
     </ProjectsSectionWrapper>
   )
