99f839d16ae82deb01724a380bf539e57c8b19e2
felix <felix@webisoft.com>
2021-03-03 14:50:38 -0500

    build new navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import {Link} from 'gatsby'
+import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap'
+import {ReactComponent as BurgerMenu} from '../images/burger-menu.svg'
+
+function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState)
+
+  return (
+    <NavbarWrapper>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle} direction="left">
+        <CustomToggle>
+          <BurgerMenu />
+        </CustomToggle>
+        <DropdownMenu>
+          <DropdownItem header>Quick Navigation</DropdownItem>
+
+          <DropdownItem divider />
+
+          <CustomItem tag={Link} to="/">
+            Home
+          </CustomItem>
+
+          <DropdownItem divider />
+
+          <DropdownItem header>Projects</DropdownItem>
+
+          <DropdownItem divider />
+
+          <CustomItem tag={Link} to="/shoppies">
+            Shoppies
+          </CustomItem>
+        </DropdownMenu>
+      </Dropdown>
+    </NavbarWrapper>
+  )
+}
+
+export default Navbar
+
+const NavbarWrapper = styled.nav`
+  position: fixed;
+  z-index: 10;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  padding: 1em;
+  height: 4em;
+`
+const CustomToggle = styled(DropdownToggle)`
+  background-color: #81a1c1;
+  border: 1px solid #81a1c1;
+
+  svg {
+    height: 36px;
+    width: 36px;
+    fill: white;
+  }
+
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: #81a1c1;
+    border: 1px solid #81a1c1;
+  }
+`
+
+const CustomItem = styled(DropdownItem)`
+  &:hover {
+    background-color: #81a1c1;
+    color: white;
+  }
+`
