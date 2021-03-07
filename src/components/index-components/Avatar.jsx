9add38773fe0b7014655af3b5fadbd7da08eddb9
felix <felix@webisoft.com>
2021-03-07 14:12:01 -0500

    fix radius

--- src/components/index-components/Avatar.jsx
+++ src/components/index-components/Avatar.jsx
@@ -5,6 +5,7 @@ function Avatar() {
   return (
     <StaticImage
       style={{borderRadius: '50%', boxShadow: '1px 1px 10px black'}}
+      imgStyle={{borderRadius: '50%'}}
       src="../../images/avatar.jpg"
       alt="Felix Headshot"
       loading="eager"
