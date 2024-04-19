cf0a9371dac15fea5c88277cc1a1a64d84438077
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-04-19 11:47:22 -0400

    remove static query component

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
-import {StaticQuery, graphql} from 'gatsby'
-import {GatsbyImage, getImage} from 'gatsby-plugin-image'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-function ModalImageComponent({image, alt}) {
+function ModalImageComponent({ image, alt }) {
   const customStyles = {
     content: {
       top: '50%',
@@ -22,14 +22,14 @@ function ModalImageComponent({image, alt}) {
   return (
     <div>
       <Modal ariaHideApp={false} isOpen={isOpen} onRequestClose={closeModal} style={customStyles}>
-        {image && <GatsbyImage image={image} style={{maxWidth: '75vw', margin: 'none'}} alt={alt} />}
+        {image && <GatsbyImage image={image} style={{ maxWidth: '75vw', margin: 'none' }} alt={alt} />}
       </Modal>
 
       {image && (
         <GatsbyImage
           onClick={() => openModal()}
           image={image}
-          style={{cursor: 'zoom-in', boxShadow: `1px 1px 10px black`, borderRadius: '5px'}}
+          style={{ cursor: 'zoom-in', boxShadow: `1px 1px 10px black`, borderRadius: '5px' }}
           alt={alt}
           aria-label="zoom on screenshot"
         />
@@ -38,28 +38,24 @@ function ModalImageComponent({image, alt}) {
   )
 }
 
-const ModalImage = (props) => (
-  <StaticQuery
-    query={graphql`
-      query MyQuery {
-        images: allFile(filter: {sourceInstanceName: {eq: "images"}}) {
-          edges {
-            node {
-              relativePath
-              childImageSharp {
-                gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-              }
+const ModalImage = (props) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      images: allFile(filter: {sourceInstanceName: {eq: "images"}}) {
+        edges {
+          node {
+            relativePath
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
             }
           }
         }
       }
-    `}
-    render={(data) => {
-      const imagePath = data.images.edges.find((image) => image.node.relativePath === props.imagePath)
-      const image = getImage(imagePath.node.childImageSharp)
-      return <ModalImageComponent image={image} {...props} />
-    }}
-  />
-)
+    }
+  `)
+  const imagePath = data.images.edges.find((image) => image.node.relativePath === props.imagePath)
+  const image = getImage(imagePath.node.childImageSharp)
+  return <ModalImageComponent image={image} {...props} />
+}
 
 export default ModalImage
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Helmet} from 'react-helmet'
-import {StaticQuery, graphql} from 'gatsby'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const detailsQuery = graphql`
   query DefaultSEOQuery {
@@ -17,46 +17,42 @@ const detailsQuery = graphql`
   }
 `
 
-function SEO({description, keywords, title, image, siteUrl, author}) {
+function SEO({ description, keywords, title, image, siteUrl, author }) {
+  const data = useStaticQuery(detailsQuery)
+  const metaDescription = description || data.site.siteMetadata.description
+  const metaTitle = title || data.site.siteMetadata.title
+  const metaAuthor = author || data.site.siteMetadata.author
+  const metaUrl = siteUrl || data.site.siteMetadata.siteUrl
+  const metaImage = image || data.site.siteMetadata.image
+  const metaKeywords = keywords || data.site.siteMetadata.keywords
+
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={(data) => {
-        const metaDescription = description || data.site.siteMetadata.description
-        const metaTitle = title || data.site.siteMetadata.title
-        const metaAuthor = author || data.site.siteMetadata.author
-        const metaUrl = siteUrl || data.site.siteMetadata.siteUrl
-        const metaImage = image || data.site.siteMetadata.image
-        const metaKeywords = keywords || data.site.siteMetadata.keywords
-        return (
-          <Helmet
-            title={title}
-            meta={[
-              {name: 'description', content: metaDescription},
-              {property: `og:title`, content: metaTitle},
-              {property: `og:description`, content: metaDescription},
-              {property: `og:type`, content: 'website'},
-              {property: `og:url`, content: metaUrl},
-              {property: `twitter:card`, content: 'summary_large_image'},
-              {property: `twitter:creator`, content: metaAuthor},
-              {property: `twitter:title`, content: metaTitle},
-              {property: `twitter:description`, content: metaDescription},
-              {property: `twitter:image`, content: metaImage},
-            ].concat(
-              metaKeywords && metaKeywords.length > 0
-                ? {
-                    name: `keywords`,
-                    content: metaKeywords.join(', '),
-                  }
-                : [],
-            )}
-          >
-            <html lang="en" />
-          </Helmet>
-        )
-      }}
-    />
+    <Helmet
+      title={title}
+      meta={[
+        { name: 'description', content: metaDescription },
+        { property: `og:title`, content: metaTitle },
+        { property: `og:description`, content: metaDescription },
+        { property: `og:type`, content: 'website' },
+        { property: `og:url`, content: metaUrl },
+        { property: `twitter:card`, content: 'summary_large_image' },
+        { property: `twitter:creator`, content: metaAuthor },
+        { property: `twitter:title`, content: metaTitle },
+        { property: `twitter:description`, content: metaDescription },
+        { property: `twitter:image`, content: metaImage },
+      ].concat(
+        metaKeywords && metaKeywords.length > 0
+          ? {
+            name: `keywords`,
+            content: metaKeywords.join(', '),
+          }
+          : [],
+      )}
+    >
+      <html lang="en" />
+    </Helmet>
   )
+
 }
 
 export default SEO
