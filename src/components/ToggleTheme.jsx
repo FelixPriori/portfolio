946dc6a7c51cfbca4ee84ba14f8b6c6332e8f21b
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-11-12 16:56:41 -0500

    add pointer on hover for switch

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import { FormGroup, Label, Input } from 'reactstrap'
+import styled from 'styled-components'
+
+const Switch = styled(FormGroup)`
+	cursor: pointer;
+`
+
+const SwitchLabel = styled(Label)`
+	cursor: inherit;
+`
+
+const SwitchInput = styled(Input)`
+	cursor: inherit;
+`
 
 export default function ToggleTheme() {
 	return (
 		<ThemeToggler>
 			{({ theme, toggleTheme }) => (
-				<FormGroup switch>
-					<Input
+				<Switch switch>
+					<SwitchInput
 						id="dark-mode-switch"
 						type="switch"
 						checked={theme === 'dark'}
 						onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
 					/>
-					<Label htmlFor="dark-mode-switch" checked={theme === 'dark'}>
+					<SwitchLabel htmlFor="dark-mode-switch" checked={theme === 'dark'}>
 						Dark Mode
-					</Label>
-				</FormGroup>
+					</SwitchLabel>
+				</Switch>
 			)}
 		</ThemeToggler>
 	)
